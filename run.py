6fe7fab88e57b434254c41b18a52143a622701b9
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2023-12-18 12:23:11 +0000

    Add Bootstrap theme through Flask

--- run.py
+++ run.py
@@ -10,9 +10,19 @@ def index():
     return render_template('index.html')
 
 
-app.route('/about')
+@app.route('/about')
 def about():
         return render_template('about.html')
+    
+    
+@app.route('/contact')
+def contact():
+        return render_template('contact.html')
+    
+    
+@app.route('/careers')
+def careers():
+        return render_template('careers.html')
 
 
 if __name__ == "__main__":
